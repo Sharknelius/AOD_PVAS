c3fcfb7e21bc2562e2eac9400e1364c5f361911a
Jase Rodriguez <jasminerodriguez590@gmail.com>
2025-03-23 20:33:19 -0400

    We got a model

--- YOLO11-Object-Tracking/ultralytics/models/yolo/detect/testModel.py
+++ YOLO11-Object-Tracking/ultralytics/models/yolo/detect/testModel.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("best.pt")
+model = YOLO("AOD_weapons.pt")
 
 # Test on an image
-results = model("test_image2.jpg", conf=0.1, show=True)
+results = model("test_image2.jpg", show=True)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
