77d3ae41de47089003f9d1d58c7e3876cccee561
Jase Rodriguez <jasminerodriguez590@gmail.com>
2025-04-13 18:04:33 -0400

    New model trained (no improvement)

--- YOLO11-Object-Tracking/ultralytics/models/yolo/detect/trainModel.py
+++ YOLO11-Object-Tracking/ultralytics/models/yolo/detect/trainModel.py
@@ -7,8 +7,8 @@ if __name__ ==  '__main__':
     #model.train(data="../../../cfg/datasets/coco-and-weapons-data.yaml", epochs=25, imgsz=640, dropout = 0.25, plots=True, batch=16, device=0)
 
     # Pretrained model
-    model = YOLO("yolo11s_AOD2.pt")
+    model = YOLO("yolo11s_AOD3.pt")
 
-    model.train(data="../../../cfg/datasets/coco-and-weapons-data.yaml", epochs=40, imgsz=640, dropout = 0.25, plots=True, batch=16, device=0)
+    model.train(data="../../../cfg/datasets/coco-and-weapons-data.yaml", epochs=50, imgsz=640, dropout = 0.25, plots=True, batch=16, device=0)
 
     model.val()
